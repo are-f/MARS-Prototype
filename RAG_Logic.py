13882bcea617635d006a2c7709138678c9d42000
amn-sdqi <amaanshahid2510@gmail.com>
2025-07-03 20:28:21 +0530

    rag fix

--- RAG_Logic.py
+++ RAG_Logic.py
@@ -3,29 +3,10 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-
-def pdf_rag(docs):
+def create_vector(docs):
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = splitter.split_documents(docs)
 
     embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    pdf_vector_store = FAISS.from_documents(chunks, embedding_model)
-    return pdf_vector_store
-
-
-def txt_rag(docs):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    chunks = splitter.split_documents(docs)
-
-    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    txt_vector_store = FAISS.from_documents(chunks, embedding_model)
-    return txt_vector_store
-
-
-def docx_rag(docs):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    chunks = splitter.split_documents(docs)
-
-    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    docx_vector_store = FAISS.from_documents(chunks, embedding_model)
-    return docx_vector_store
+    vector_store = FAISS.from_documents(chunks, embedding_model)
+    return vector_store
--- doc_loader.py
+++ doc_loader.py
@@ -1,24 +1,24 @@
 from langchain_community.document_loaders import PyMuPDFLoader,UnstructuredWordDocumentLoader,TextLoader
-from RAG_Logic import pdf_rag,docx_rag,txt_rag
+from RAG_Logic import create_vector
 
 
 
 def pdf_loader(file_path):
     loader = PyMuPDFLoader(file_path=file_path)
     docs = loader.load()
-    pdf_vector_store=pdf_rag(docs) 
+    pdf_vector_store = create_vector(docs) 
     return pdf_vector_store 
     
     
 def docx_loader(file_path):
     loader = UnstructuredWordDocumentLoader(file_path=file_path)
     docs = loader.load()
-    docx_vector_store = docx_rag(docs)
+    docx_vector_store = create_vector(docs)
     return docx_vector_store
     
 def txt_loader(file_path):
     loader = TextLoader(file_path=file_path)
     docs = loader.load()
-    text_vector_store = txt_rag(docs)
+    text_vector_store = create_vector(docs)
     return text_vector_store
 
